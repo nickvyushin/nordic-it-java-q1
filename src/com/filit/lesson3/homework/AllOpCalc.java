212bdaff8c885c8490877fa1c681c4049dbf5d6a
nickvyushin <nickvyushin@gmail.com>
2020-03-04 14:11:06 +0300

    Исправления замечаний по заданию калькулятор.

--- src/com/filit/lesson3/homework/AllOpCalc.java
+++ src/com/filit/lesson3/homework/AllOpCalc.java
@@ -8,7 +8,13 @@ public class AllOpCalc {
         System.out.println("Введите операцию: ");
         var op = sc.next();
 
-        while (!op.equals("+") && !op.equals("-") && !op.equals("/") && !op.equals("*") && !op.equals("div") && !op.equals("mod") && !op.equals("^")) {
+        while (!op.equals("+")
+                && !op.equals("-")
+                && !op.equals("/")
+                && !op.equals("*")
+                && !op.equals("div")
+                && !op.equals("mod")
+                && !op.equals("^")) {
             System.out.println("Некорректная операция! Введите одну из доступных операций: ");
             op = sc.next();
         }
@@ -29,16 +35,16 @@ public class AllOpCalc {
                 if (num2 == 0) {
                     System.out.println("На ноль делить нельзя!");
                 } else {
-                    System.out.println("Результат деления: " + (num1 / num2));
+                    double a = num1;
+                    double b = num2;
+                    System.out.println("Результат деления: " + (a / b));
                 }
                 break;
             case "*":
                 System.out.println("Результат умножения: " + (num1 * num2));
                 break;
             case "div":
-                var div = num1 % num2;
-                var res = (num1 - div) / num2;
-                System.out.println("Результат целочисленного деления: " + res);
+                System.out.println("Результат целочисленного деления: " + (num1 / num2));
                 break;
             case "mod":
                 System.out.println("Остаток от деления: " + (num1 % num2));
@@ -47,12 +53,12 @@ public class AllOpCalc {
                 if (num1 == 0 && num2 < 0) {
                     System.out.println("Бесконечность");
                 } else if (num2 < 0) {
-                    var result = 1;
+                    double result = 1;
                     num2 = Math.abs(num2);
                     for (var i = 1; i <= num2; i++) {
                         result = result * num1;
                     }
-                    System.out.println("Результат возведения в степень: " + (1 / result));
+                    System.out.println("Результат возведения в степень: " + (1.0 / result));
                 } else {
                     var result = 1;
                     for (var i = 1; i <= num2; i++) {
