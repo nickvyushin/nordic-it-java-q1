932af38a2d5e50662aeb78e6a770c091e6f19373
nickvyushin <nickvyushin@gmail.com>
2020-12-07 23:18:41 +0300

    Edited HomeWork

--- src/com/filit/lesson17/homework/Factory.java
+++ src/com/filit/lesson17/homework/Factory.java
@@ -2,7 +2,6 @@ package com.filit.lesson17.homework;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Factory {
     public static void main(String[] args) throws InterruptedException {
@@ -11,7 +10,7 @@ public class Factory {
         var processed = new ArrayBlockingQueue<Integer>(20);
         var executor = Executors.newFixedThreadPool(5);
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 20; i++) {
             notProcessed.add(i);
         }
 
@@ -29,19 +28,23 @@ public class Factory {
         executor.execute(process5);
         executor.execute(process6);
 
-        executor.awaitTermination(10, TimeUnit.SECONDS);
-
-        executor.shutdown();
-
         process1.start();
         process2.start();
         process3.join();
+
         process3.start();
         process4.join();
         process5.join();
+
         process4.start();
         process5.start();
         process6.join();
+
         process6.start();
+
+        System.out.println("Обработка завершена: ");
+        System.out.println(notProcessed.size());
+        System.out.println(processed);
+        System.out.println(processed.size());
     }
 }
